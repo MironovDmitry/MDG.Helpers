aa7851beb7eb91d909a7581bb5244cb3b3b52761
Dmitry Mironov <dmitry.g.mironov@gmail.com>
2015-03-23 11:20:10 +0300

    Added GetDateFromWeekNumberAndDayOfWeek method

---
+++ MDG.Helpers/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDG.Helpers
+{
+    public static class DateAndTime
+    {
+        public static DateTime GetDateFromWeekNumberAndDayOfWeek(int weekNumber, int dayOfWeek)
+        {
+            DateTime jan1 = new DateTime(DateTime.Now.Year, 1, 1);
+            int daysOffset = DayOfWeek.Tuesday - jan1.DayOfWeek;
+
+            DateTime firstMonday = jan1.AddDays(daysOffset);
+
+            var cal = CultureInfo.CurrentCulture.Calendar;
+            int firstWeek = cal.GetWeekOfYear(jan1, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+            var weekNum = weekNumber;
+            if (firstWeek <= 1)
+            {
+                weekNum -= 1;
+            }
+
+            var result = firstMonday.AddDays(weekNum * 7 + dayOfWeek - 1);
+            return result;
+        } 
+    }
+}
